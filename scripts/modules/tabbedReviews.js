6eafae1da45c5f1513388708d3c3649aa72196c0
Helena Avis <helenaavis48@gmail.com>
2025-02-21 10:04:41 +0000

    Added functionality for buttons to switch the currently visible review. Also added new and more relevant reviews.

--- scripts/modules/tabbedReviews.js
+++ scripts/modules/tabbedReviews.js
@@ -1,6 +1,26 @@
-function changeReview() {
-    // Find the index of the button that was clicked
-    const buttons = this.parentElement.children;
+const reviewButtonContainer = document.querySelector(".review-button-container");
+const reviewContainer = document.querySelector(".reviews-spotlight-section");
 
+function changeReview() {
+    // Swith selected tab
+    reviewButtonContainer.querySelector(".selected").classList.remove("selected");
+    this.classList.add("selected");
+    
+    // Hide currently visible review
+    document.querySelector(".visible").classList.remove("visible");
+    // Find the index of the button that was clicked, and use it to find the relevant review
+    reviewContainer.children[Array.from(reviewButtonContainer.children).indexOf(this) + 1].classList.add("visible");
     
 }
+
+function addButtonEventListeners() {
+    // for (const button in reviewButtonContainer.children) {
+    //     button.addEventListener("click", changeReview);
+    // }
+
+    for (let i = 0; i < reviewButtonContainer.children.length; i++) {
+        reviewButtonContainer.children[i].addEventListener("click", changeReview);
+    }
+}
+
+addButtonEventListeners();
